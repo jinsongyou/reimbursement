067a36abc143507ffce7a7b02ffdaefa077f03aa
jinsongyou <jinsongyou@163.com>
2018-02-06 14:00:07 +0800

    augular finish test

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,31 +1,31 @@
 var app = angular.module('customers',[]);
 
 app.controller("CustomerSearchController",[
-  "$scope",
-  function($scope){
+  "$scope","$http",
+  function($scope,$http){
+    var page = 0;
     $scope.search = function(searchTerm){
       $scope.searchedFor = searchTerm;
       $scope.customers = [];
-      $scope.customers = [
-        {
-          "first_name":"Schuyler",
-          "last_name":"Cremin",
-          "email":"gidild@dihkhhh.com",
-          "created_at":"2018-01-08",
-        },
-        {
-          "first_name":"Your Han",
-          "last_name":"Crymon",
-          "email":"ge@dc.com",
-          "created_at":"2018-02-08",
-        },
-        {
-          "first_name":"Hope",
-          "last_name":"srymon",
-          "email":"ge5@dc.com",
-          "created_at":"2018-02-09",
-        }
-      ]
+      $http.get("/customers.json",
+                {"params":{ "keywords": searchTerm, "page": page }}
+              ).then(function(response){
+                $scope.customers = response.data;
+              },function(response){
+                alert("There was a problem: " + response.status);
+              });
     }
+
+    $scope.previousPage=function(){
+      if (page > 0){
+          page = page -1;
+          $scope.search($scope.keywords);
+         }
+   }
+
+  $scope.nextPage=function(){
+         page = page + 1;
+        $scope.search($scope.keywords);
+      }
   }
 ]);
